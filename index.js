b6cd253d555241647730ebc769d27db21b897eb5
delightful-certificate-3580 <frenchgod@gmail.com>
2018-11-18 18:08:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 function dwarfRollCall(dwarves) {
+  	var dwarfString = ""
+	for (var i = 0; i < dwarves.length; i++){
+		dwarfString = dwarfString + (i + 1 + ". " + dwarves[i] + " ") 
+	}
+
+return dwarfString	
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(listOfPowers){
+  var planetList = []
+	for (var i = 0; i < listOfPowers.length ; i++) {
+		 planetList.push(listOfPowers[i].toUpperCase() + "!")
+	}	
+
+return planetList
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(listOfPowers) {
+  for (var i = 0; i < listOfPowers.length; i++){
+		if (listOfPowers[i].length > 4){
+			return true
+		}
+		
+	}
+	return false
 }
 
-function findTheCheese (foods) {
+function findTheCheese (shoppingCart) {
+  for (var i = 0; i < shoppingCart.length; i++) {
+		if (shoppingCart[i] === "gouda" || shoppingCart[i] === "cheddar" || shoppingCart[i] === "camembert")
+			return (shoppingCart[i])
+
+	}
+	return "no cheese!"
+
 }
